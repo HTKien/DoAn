04679756f83500ad27b2d88074db3f1f2e7c1993
hantrungkien <hantrungkien98@gmail.com>
2021-05-05 02:21:57 +0700

    done

---
+++ VIEW/Scripts/Project/admin_dashboard.js
@@ -0,0 +1,99 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        method: 'GET',
+        url: '/classes',
+        dataType: 'json',
+        async: false,
+        success: function (res) {
+            $('#total_class').text(res.length)
+        },
+        error: function (res) {
+            alert("Hệ thống đang bị lỗi!");
+        }
+    });
+    $.ajax({
+        method: 'GET',
+        url: '/bonusStudents',
+        dataType: 'json',
+        async: false,
+        success: function (res) {
+            $('#bonus').text(res.length)
+        },
+        error: function (res) {
+            alert("Hệ thống đang bị lỗi!");
+        }
+    });
+    $.ajax({
+        method: 'GET',
+        url: '/critics',
+        dataType: 'json',
+        async: false,
+        success: function (res) {
+            $('#critic').text(res.length)
+        },
+        error: function (res) {
+            alert("Hệ thống đang bị lỗi!");
+        }
+    });
+    $.ajax({
+        method: 'GET',
+        url: '/teachers',
+        dataType: 'json',
+        async: false,
+        success: function (res) {
+            $('#total_teacher').text(res.length)
+        },
+        error: function (res) {
+            alert("Hệ thống đang bị lỗi!");
+        }
+    });
+    $.ajax({
+        method: 'GET',
+        url: '/students',
+        dataType: 'json',
+        async: false,
+        success: function (res) {
+            $('#total_student').text(res.length);
+            var conduct_good = 0;
+            var conduct_kha = 0;
+            var conduct_tb = 0;
+            var conduct_yeu = 0;
+            for (var k = 0; k < res.length; k++) {
+                $.ajax({
+                    method: 'GET',
+                    url: '/students/' + res[k].StudentID,
+                    dataType: 'json',
+                    async: false,
+                    success: function (ress) {
+                        if (ress.Conduct == "Tốt") {
+                            conduct_good = conduct_good+1;
+                        }
+                        if (ress.Conduct == "Khá") {
+                            conduct_kha = conduct_kha + 1;
+                        }
+                        if (ress.Conduct == "Trung bình") {
+                            conduct_tb = conduct_tb + 1;
+                        }
+                        if (ress.Conduct == "Yếu") {
+                            conduct_yeu = conduct_yeu + 1;
+                        }
+                    },
+                    error: function (ress) {
+                        alert("Hệ thống đang bị lỗi!");
+                    }
+                });
+            }
+            $('#conduct_good').text(conduct_good + " học sinh")
+            $('#conduct_kha').text(conduct_kha + " học sinh")
+            $('#conduct_tb').text(conduct_tb + " học sinh")
+            $('#conduct_yeu').text(conduct_yeu + " học sinh")
+
+
+
+            
+        },
+        error: function (res) {
+            alert("Hệ thống đang bị lỗi!");
+        }
+    });
+})
---
+++ VIEW/Scripts/Project/teacher_dashboard.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    var classID;
+    let classIDStorageGet = localStorage.getItem('classIDStorage') ? JSON.parse(localStorage.getItem('classIDStorage')) : [];
+    classID = classIDStorageGet[classIDStorageGet.length - 1].id;
+    $.ajax({
+        method: 'GET',
+        url: '/classes/' + classID,
+        dataType: 'json',
+        async: false,
+        success: function (res) {
+            $('#class_title').text("Mã lớp: "+ res.Code+ "- Tên lớp: "+ res.Name)
+        },
+        error: function (res) {
+            alert("Hệ thống đang bị lỗi!");
+        }
+    })
+    
+})
